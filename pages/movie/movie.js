4b3dedfe36a1cff1e1b03c4a7b95bad2092196ff
fengyunlsm <2399548030@qq.com>
2019-12-16 09:24:15 +0800

    add more

--- pages/movie/movie.js
+++ pages/movie/movie.js
@@ -4,7 +4,10 @@ const app = getApp()
 
 Page({
   data: {
-    in_theaters: []
+    in_theaters: [],
+    top250: [],
+    coming_soon: [],
+    isSearch: true
   },
   onLoad: function (options) {
     let that = this
@@ -13,14 +16,46 @@ Page({
       url: 'http://t.yushu.im/v2/movie/in_theaters',
       data: {
         start: 1,
-        count: 3,
+        count: 5,
+      },
+      success: function (res) {
+        that.getStar(res.data.subjects)
+        // res.data.subjects = {'type': '正在热映'}
+        // 十点半的地铁 终于每个人都有了
+        that.setData ({
+          'in_theaters': res.data.subjects
+        }, () => {
+          console.log('isSearch: ',that.data.isSearch)
+        })
+      }
+    })
+    wx.request({
+      url: 'http://t.yushu.im/v2/movie/top250',
+      data: {
+        start: 1,
+        count: 5,
       },
       success: function (res) {
         that.getStar(res.data.subjects)
         that.setData ({
-          in_theaters: res.data.subjects
+          'top250': res.data.subjects
         }, () => {
-          console.log('reeeeee: ',that.data.in_theaters)
+          console.log('reeeeee: ',that.data.top250)
+        })
+      }
+    })
+    wx.request({
+      url: 'http://t.yushu.im/v2/movie/coming_soon',
+      data: {
+        start: 1,
+        count: 5,
+      },
+      success: function (res) {
+        that.getStar(res.data.subjects)
+        that.setData ({
+          'coming_soon': res.data.subjects
+        }, () => {
+          console.log('reeeeee: ',that.data.coming_soon)
         })
       }
     })
@@ -69,11 +104,47 @@ Page({
   },
   goMoviesDetail: function (options) {
     // 跳转到详情页
-    console.log('Detail: ', options)
     app.movieId = options.currentTarget.dataset.movieid
     console.log('movieId: ', app.movieId)
     wx.navigateTo({
       url: "../movie_detail/movie_detail"
+    })
+  },
+  goMovieList: function (options) {
+    // 跳转到列表页
+    // http://t.yushu.im/v2/movie/in_theaters
+    // http://t.yushu.im/v2/movie/top250?statr=0&count=3
+    // http://t.yushu.im/v2/movie/coming_soon?statr=0&count=3
+    console.log('跳转到列表详情页')
+    let route = {
+      '1': 'in_theaters',
+      '2': 'top250',
+      '3': 'coming_soon'
+    }
+    console.log('optionsID:', options)
+    let movieListId = options.currentTarget.dataset.movielistid
+    console.log('movieListId: ', movieListId)
+    app.movieListRoute = route[movieListId]
+    wx.navigateTo({
+      url: "../movie_list/movie_list"
+    })
+  },
+  search: function () {
+    // 修改样式
+    console.log('修改了样式')   // weiziji1
+    this.setData({
+      isSearch: false
+    }, () => {
+      console.log('样式:', this.data.isSearch)
+    })
+  },
+  cancelSearch: function () {
+
+    console.log('确认搜索')
+    this.setData({
+      isSearch: true
+    }, () => {
+      console.log('cancel：', this.data.isSearch)
     })
   }
 })
